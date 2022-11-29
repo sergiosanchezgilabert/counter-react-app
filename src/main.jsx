12a5fff98e0f38785439bba1dba2344c4ed8fb51
sergio.sanchez <sergio.sanchez@bosonit.com>
2022-11-29 16:25:53 +0100

    Style .css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 
+import './styles.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <FirstApp />
