3a65289221ffd9fbe897f140b4220720666b4886
sergio.sanchez <sergio.sanchez@bosonit.com>
2022-11-30 16:46:50 +0100

    useState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,9 +1,22 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 export const CounterApp = ({ value }) => {
+
+    const [counter, setCounter] = useState(value)
+
+    const sumar = () => {
+        //setCounter(counter + 1)
+        setCounter((c) => c + 1)//c tiene el valor counter actual, le suma 1 y carga counter con setCounter
+    }
+
     return <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2>{counter}</h2>
+
+        <button onClick={sumar}>
+            +1
+        </button>
     </>
 }
 
