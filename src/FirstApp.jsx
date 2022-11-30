b5cac791b3a7252ca4e6be3722b82f83298a47c3
sergio.sanchez <sergio.sanchez@bosonit.com>
2022-11-30 10:11:24 +0100

    DefaultProps

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,14 +1,20 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types' //yarn add prop-types
 
-export const FirstApp = ({ title, subtitle }) => {
+export const FirstApp = ({ title, subtitle, inf }) => {
     return (
         <>
             <h1>{title}</h1>
             <h2>Y tengo {subtitle} años</h2>
+            <h3>{inf}</h3>
         </>)
 }
 
 FirstApp.propTypes = {
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+    title: 'No hay titulo',
+    inf: 'Informacion interna'
 }
