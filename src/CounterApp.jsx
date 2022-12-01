3915f7e986f711ad5404fc340b9101c61b408aaa
sergio.sanchez <sergio.sanchez@bosonit.com>
2022-12-01 11:18:34 +0100

    useState en resta y reseteo

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,18 +5,23 @@ export const CounterApp = ({ value }) => {
 
     const [counter, setCounter] = useState(value)
 
+    //Cada vez que se llama a un funcion, el componente se ejecuta de nuevo
     const sumar = () => {
         //setCounter(counter + 1)
         setCounter((c) => c + 1)//c tiene el valor counter actual, le suma 1 y carga counter con setCounter
     }
 
+    const restar = () => setCounter((c) => c - 1)
+
+    const resetear = () => setCounter((c) => value)//c tiene el valor counter actual, le suma 1 y carga counter con setCounter
+
     return <>
         <h1>CounterApp</h1>
         <h2>{counter}</h2>
 
-        <button onClick={sumar}>
-            +1
-        </button>
+        <button onClick={sumar}>+1</button>
+        <button onClick={restar}>-1</button>
+        <button onClick={resetear}>Reset</button>
     </>
 }
 
