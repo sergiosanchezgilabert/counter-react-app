30e8a88e0a03dd907685fa19f9d039f266a13c1c
sergio.sanchez <sergio.sanchez@bosonit.com>
2023-01-09 10:55:55 +0100

    Primera ejecucion de test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,3 +1,15 @@
-test('Esta prueba no debe de fallar ', () => {
+//Instalar jest -> yarn add --dev jest
+describe('Pruebas', () => {
+    test('Esta prueba no debe de fallar ', () => { //Ejecutar el test -> yarn test 
+
+        // 1. inicializacion
+        const message1 = 'Hola Mundo'
+
+        // 2. estimulo
+        const message2 = message1.trim()//Quita espacios en blanco al principio y final de la cadena
+
+        // 3. Observar el comportamiento esperado
+        expect(message1).toBe(message2) //yarn add -D @types/jest0
+    })
 
 })
